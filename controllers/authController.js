37746d76ea2bee5a8d3fe8805e8b42d828a1973b
javid shabin <javid.main.act@gmail.com>
2025-07-20 21:22:30 +0530

    Check the user is already existe or not by email and hash the user password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,22 @@
 import { userSignupValidation } from "../config/authValidations.js";
+import userModel from "../models/userModel.js";
+import { AppError } from "../utils/AppError.js";
+import { hashPassword } from "../utils/hashPassword.js";
 
 export const signupUser = async (req, res, next) => {
   try {
     // Validate the user details
     userSignupValidation(req.body)
     // Destructer user details from request body
-    const {userName, email, password, confirmPassword, phone} = req.body
+    const {userName, email, password, phone} = req.body
+    // Check the user details in database
+    // Find the user from database
+    const isUserExist = await userModel.findOne({email})
+    if (isUserExist) {
+      throw new AppError("User already exist", 404)
+    }
+    // Hash the user password 10 round salting using bcrypt
+    const hashedPassword = await hashPassword(password)
     
   } catch (error) {
     next(error)
