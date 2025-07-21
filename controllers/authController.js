572a4b490f22f9a69638add0ce8da4b1ba8bead3
javid shabin <javid.main.act@gmail.com>
2025-07-21 21:56:13 +0530

    complete the user singup function and generate token using jwt , save to the token to cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 import { userSignupValidation } from "../config/authValidations.js";
 import userModel from "../models/userModel.js";
 import { AppError } from "../utils/AppError.js";
+import { generateToken } from "../utils/generateToken.js";
 import { hashPassword } from "../utils/hashPassword.js";
 
 export const signupUser = async (req, res, next) => {
@@ -8,7 +9,7 @@ export const signupUser = async (req, res, next) => {
     // Validate the user details
     userSignupValidation(req.body)
     // Destructer user details from request body
-    const {userName, email, password, phone} = req.body
+    const {userName, email, password, phone, role} = req.body
     // Check the user details in database
     // Find the user from database
     const isUserExist = await userModel.findOne({email})
@@ -18,6 +19,31 @@ export const signupUser = async (req, res, next) => {
     // Hash the user password 10 round salting using bcrypt
     const hashedPassword = await hashPassword(password)
     
+    // Create new user
+    const newUser = new userModel({
+      userName,
+      email,
+      password: hashedPassword,
+      phone,
+      role
+    })
+    await newUser.save()
+    //Token creation
+    const token = generateToken({
+      id: newUser.id,
+      email: newUser.email,
+      role: newUser.role,
+    });
+    res.cookie("userToken", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "strict",
+    });
+    
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+    });
   } catch (error) {
     next(error)
   }
