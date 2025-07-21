628c07422dcda4e18a10eaf3faae1cd088d5b3aa
javid shabin <javid.main.act@gmail.com>
2025-07-21 20:56:33 +0530

    Create validation function for user authentication

---
+++ config/authValidations.js
@@ -0,0 +1,13 @@
+// ************ Auth validations *****************
+
+import { AppError } from "../utils/AppError";
+
+// User signup validation
+export const userSignupValidation = (data) => {
+  // Destructer user details from the data
+  const { userName, email, password, confirmPassword, phone } = data;
+  // Check the required fileds are present or not
+  if (!userName || !email || !password || !confirmPassword || !phone) {
+    throw new AppError("All fields are required", 400);
+  }
+};
