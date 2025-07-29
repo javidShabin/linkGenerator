a699666b22c3084c8cc37faf64d14e66bb87145e
javid shabin <javid.main.act@gmail.com>
2025-07-30 00:23:38 +0530

    Impliment the link usage tracking ufnction

--- routes/linkRouters.js
+++ routes/linkRouters.js
@@ -11,23 +11,26 @@ import { authenticate } from "../middlewares/auth.middleware.js";
 import { authorize } from "../middlewares/authorize.js";
 const router = express();
 
-
-
 router.post("/create-link", authenticate, authorize("user", "pro"), createLink);
+
 router.get(
   "/get-prev-links",
   authenticate,
   authorize("user", "pro"),
   getPrevLinks
 );
+
 router.get("/get-latest-link/:userId", getLatestLink)
+
 router.get("/track-link/:slug", trachLinkUsage);
+
 router.put(
   "/update-link/:slug",
   authenticate,
   authorize("user", "pro"),
   updateLink
 );
+
 router.delete("/delete-link/:slug", deleteLink);
 
 export default router;
