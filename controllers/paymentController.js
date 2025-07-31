54a294733171e10e89597e2c6883fa00c8533b01
javid shabin <javid.main.act@gmail.com>
2025-07-31 16:15:01 +0530

    Add get all payment detaisl function

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -108,13 +108,18 @@ export const getStripeSessionDetails = async (req, res, next) => {
 
 export const getAllPaymentDetails = async (req, res, next) => {
   try {
-    const getAllPayments = await paymentModel.find({});
-    if (!getAllPayments) {
+    const getAllPayments = await paymentModel
+      .find({})
+      .populate("userId", " email userName");
+
+    if (!getAllPayments || getAllPayments.length === 0) {
       throw new AppError("Payment details not available", 404);
     }
-    res
-      .status(200)
-      .json({ message: "Payment details fetched", data: getAllPayments });
+
+    res.status(200).json({
+      message: "Payment details fetched",
+      data: getAllPayments,
+    });
   } catch (error) {
     next(error);
   }
